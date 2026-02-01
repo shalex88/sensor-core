b3457dcba26383c786337dbeddb1f52c476745de
Alex Sh <shalex.work@gmail.com>
2026-02-01 17:52:35 +0200

    Compile successfully

--- src/infrastructure/camera/protocol/genicam/include/_GenICamVersion.h
+++
@@ -1,9 +0,0 @@
-//  This file is generated automatically. Do not modify! 
-#define GENICAM_VERSION_MAJOR 3 
-#define GENICAM_VERSION_MINOR 5 
-#define GENICAM_VERSION_SUBMINOR 0
-
-#define GENICAM_MAIN_COMPILER 
-
-/* #undef GENICAM_COMPANY_SUFFIX */
-/* #undef GENICAM_SVN_REVISION */
--- src/infrastructure/fpga/VideoChannel.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace service::infrastructure {
-    class VideoChannel {
-    public:
-        explicit VideoChannel(int channel_num);
-        ~VideoChannel() noexcept;
-    };
-} // namespace service::infrastructure
